e0ed97c455ee6169c148902095f7b95ae2fe2e8b
Rene Brun <Rene.Brun@cern.ch>
2002-10-05 07:28:54 +0000

    geant3 type corrected to TGeant3* (P. Hristov)

--- examples/E02/g3Config.C
+++ examples/E02/g3Config.C
@@ -1,10 +1,10 @@
-// $Id: $
+// $Id: g3Config.C,v 1.1.1.1 2002/09/27 10:00:02 rdm Exp $
 //
 // Configuration macro for Geant3 VirtualMC for Example01 
 
 void Config()
 {
-  TGeant3 geant3
+  TGeant3* geant3
     = new  TGeant3("C++ Interface to Geant3"); 
   
   cout << "Geant3 has been created." << endl;
