aaf7e8ac339b594be9a82420c9a14019121b5c8c
Rene Brun <Rene.Brun@cern.ch>
2003-06-03 17:11:42 +0000

    From Ivana: removed commands for selecting activated physics constructors (moved to TG4PhysicsListMessenger)

--- source/physics/src/TG4PhysicsMessenger.cxx
+++ source/physics/src/TG4PhysicsMessenger.cxx
@@ -1,4 +1,4 @@
-// $Id: TG4PhysicsMessenger.cxx,v 1.1.1.1 2002/09/27 10:00:03 rdm Exp $
+// $Id: TG4PhysicsMessenger.cxx,v 1.2 2002/12/18 09:35:32 brun Exp $
 // Category: physics
 //
 // Author: I. Hrivnacova
@@ -8,19 +8,13 @@
 // See the class description in the header file.
 
 #include "TG4PhysicsMessenger.h"
-#include "TG4ModularPhysicsList.h"
 #include "TG4GeometryServices.h"
-#include "TG4PhysicsManager.h"
 #include "TG4G3PhysicsManager.h"
-#include "TG4G3CutVector.h"
-#include "TG4G3ControlVector.h"
 #include "TG4ProcessControlMap.h"
 #include "TG4ProcessMCMap.h"
 
 #include <G4UIdirectory.hh>
 #include <G4UIcmdWithoutParameter.hh>
-#include <G4UIcmdWithABool.hh>
-#include <G4UIcmdWithADouble.hh>
 #include <G4UIcmdWithAString.hh>
 
 //_____________________________________________________________________________
@@ -30,51 +24,6 @@ TG4PhysicsMessenger::TG4PhysicsMessenger(TG4PhysicsManager* physicsManager)
 //
   fDirectory = new G4UIdirectory("/mcPhysics/");
   fDirectory->SetGuidance("TGeant4 physics control commands.");
-
-  fSetEMCmd
-     = new G4UIcmdWithABool("/mcPhysics/setEM", this);
-  fSetEMCmd->SetGuidance("Set electromagnetic physics.");
-  fSetEMCmd->SetParameterName("EMControl", false);
-  fSetEMCmd->AvailableForStates(G4State_PreInit);
-
-  fSetMuonCmd
-     = new G4UIcmdWithABool("/mcPhysics/setMuon", this);
-  fSetMuonCmd->SetGuidance("Set muon physics.");
-  fSetMuonCmd->SetParameterName("EMControl", false);
-  fSetMuonCmd->AvailableForStates(G4State_PreInit);
-
-  fSetHadronCmd
-     = new G4UIcmdWithABool("/mcPhysics/setHadron", this);
-  fSetHadronCmd->SetGuidance("Set hadron physics.");
-  fSetHadronCmd->SetParameterName("HadronControl", false);
-  fSetHadronCmd->AvailableForStates(G4State_PreInit);
-
-  fSetOpticalCmd
-     = new G4UIcmdWithABool("/mcPhysics/setOptical", this);
-  fSetOpticalCmd->SetGuidance("Set Cerenkov and optical physics.");
-  fSetOpticalCmd->SetParameterName("OpticalControl", false);
-  fSetOpticalCmd->AvailableForStates(G4State_PreInit);
-
-  fSetSpecialCutsCmd
-     = new G4UIcmdWithABool("/mcPhysics/setSpecialCuts", this);
-  fSetSpecialCutsCmd->SetGuidance("Set special cuts process.");
-  fSetSpecialCutsCmd
-    ->SetGuidance("!! Support for this option is under development.");
-  fSetSpecialCutsCmd->SetParameterName("SpecialCutsControl", false);
-  fSetSpecialCutsCmd->AvailableForStates(G4State_PreInit);
-
-  fSetSpecialControlsCmd
-     = new G4UIcmdWithABool("/mcPhysics/setSpecialControls", this);
-  fSetSpecialControlsCmd->SetGuidance("Set special controls process.");
-  fSetSpecialControlsCmd
-    ->SetGuidance("!! Support for this option is under development.");
-  fSetSpecialControlsCmd->SetParameterName("SpecialFlagsControl", false);
-  fSetSpecialControlsCmd->AvailableForStates(G4State_PreInit);
-
-  fRangeCutCmd
-     = new G4UIcmdWithADouble("/mcPhysics/rangeCut", this);
-  fRangeCutCmd->SetGuidance("Sets the global cut in range (in mm)");
-  fRangeCutCmd->AvailableForStates(G4State_PreInit);
 
   fPrintProcessMCMapCmd
      = new G4UIcmdWithoutParameter("/mcPhysics/printProcessMCMap", this);
@@ -124,14 +73,6 @@ TG4PhysicsMessenger::~TG4PhysicsMessenger() {
 //
 
   delete fDirectory;
-  delete fSetEMCmd;
-  delete fSetMuonCmd;
-  delete fSetHadronCmd;
-  delete fSetOpticalCmd;
-  delete fSetSpecialCutsCmd;
-  delete fSetSpecialControlsCmd;
-  delete fRangeCutCmd;
-  delete fProcessActivationCmd;
   delete fPrintProcessMCMapCmd;
   delete fPrintProcessControlMapCmd;
   delete fPrintVolumeLimitsCmd;
@@ -160,40 +101,7 @@ void TG4PhysicsMessenger::SetNewValue(G4UIcommand* command, G4String newValue)
 // Applies command to the associated object.
 // ---
 
-  if (command == fSetEMCmd) {
-    fPhysicsManager
-      ->SetEMPhysics(fSetEMCmd->GetNewBoolValue(newValue)); 
-  }    
-  if (command == fSetMuonCmd) {
-    fPhysicsManager
-      ->SetMuonPhysics(fSetMuonCmd->GetNewBoolValue(newValue)); 
-  }    
-  else if (command == fSetHadronCmd) {
-    fPhysicsManager
-      ->SetHadronPhysics(fSetHadronCmd->GetNewBoolValue(newValue)); 
-  }    
-  else if (command == fSetOpticalCmd) {
-    fPhysicsManager
-      ->SetOpticalPhysics(fSetOpticalCmd->GetNewBoolValue(newValue)); 
-  }    
-  else if (command == fSetSpecialCutsCmd) {
-    fPhysicsManager
-      ->SetSpecialCutsPhysics(fSetSpecialCutsCmd->GetNewBoolValue(newValue)); 
-  }    
-  else if (command == fSetSpecialControlsCmd) {
-    fPhysicsManager
-      ->SetSpecialControlsPhysics(
-          fSetSpecialControlsCmd->GetNewBoolValue(newValue)); 
-  }    
-  else if (command == fRangeCutCmd) {
-    fPhysicsManager
-      ->GetPhysicsList()
-        ->SetRangeCut(fRangeCutCmd->GetNewDoubleValue(newValue));
-  }  
-  else if (command == fProcessActivationCmd) {
-    fPhysicsManager->SetProcessActivation();
-  }  
-  else if (command == fPrintProcessMCMapCmd) {
+  if (command == fPrintProcessMCMapCmd) {
     TG4ProcessMCMap::Instance()->PrintAll();
   }  
   else if (command == fPrintProcessControlMapCmd) {
