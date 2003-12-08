13d3563e5eedb3b586b6176399a2b341d8a5f7b4
Rene Brun <Rene.Brun@cern.ch>
2003-12-08 14:13:40 +0000

    New version from Ivana

--- examples/macro/g3libs.C
+++ examples/macro/g3libs.C
@@ -1,14 +1,27 @@
-// $Id: $
+// $Id: g3libs.C,v 1.1.1.1 2002/09/27 10:00:02 rdm Exp $
 //
 // Macro for loading Geant3 libraries
 
 #include <iostream>
 
+Bool_t isLibrary(const char* libName)
+{
+  if (TString(gSystem->DynamicPathName(libName, kTRUE)) != TString(""))
+    return kTRUE;
+  else  
+    return kFALSE;
+}    
+
 void g3libs()
 {
-  cout << "Loading Geant3 library ..." << endl;
+  cout << "Loading Geant3 libraries ..." << endl;
+
+  if (isLibrary("libdummies.so"))
+     gSystem->Load("libdummies.so");
+                   // libdummies.so needed from geant3_+vmc version 0.5
 
   gSystem->Load("libgeant321.so");
 
-  cout << "Loading Geant3 library ... finished" << endl;
+  cout << "Loading Geant3 libraries ... finished" << endl;
 }
+
