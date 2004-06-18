6a92901464558048cec7aa6e9ffeef94a80739e2
Rene Brun <Rene.Brun@cern.ch>
2004-06-18 13:52:03 +0000

    From Ivana: Removed obsolete tests of environment variables for graphics drivers not needing external packages or libraries; updated according to ExN03VisManager.cc (Geant4 release 6.1).

--- source/visualization/src/TG4VisManager.cxx
+++ source/visualization/src/TG4VisManager.cxx
@@ -1,4 +1,4 @@
-// $Id: TG4VisManager.cxx,v 1.1.1.1 2002/06/16 15:57:36 hristov Exp $
+// $Id: TG4VisManager.cxx,v 1.1.1.1 2002/09/27 10:00:03 rdm Exp $
 // Category: visualization
 //
 // Author: I. Hrivnacova, A. Gheata
@@ -49,27 +49,25 @@
 
 // Supported drivers...
 
+// Not needing external packages or libraries...
+#include "G4ASCIITree.hh"
+#include "G4DAWNFILE.hh"
+#include "G4GAGTree.hh"
+#include "G4HepRepFile.hh"
+#include "G4HepRep.hh"
+#include "G4RayTracer.hh"
+#include "G4VRML1File.hh"
+#include "G4VRML2File.hh"
+
+// Needing external packages or libraries...
+
 #ifdef G4VIS_USE_DAWN
-#include <G4FukuiRenderer.hh>
-#endif
-
-#ifdef G4VIS_USE_DAWNFILE
-#include <G4DAWNFILE.hh>
-#endif
-
-#ifdef G4VIS_USE_OPACS
-#include <G4Wo.hh>
-#include <G4Xo.hh>
+#include "G4FukuiRenderer.hh"
 #endif
 
 #ifdef G4VIS_USE_OPENGLX
-#include <G4OpenGLImmediateX.hh>
-#include <G4OpenGLStoredX.hh>
-#endif
-
-#ifdef G4VIS_USE_OPENGLXM
-#include <G4OpenGLImmediateXm.hh>
-#include <G4OpenGLStoredXm.hh>
+#include "G4OpenGLImmediateX.hh"
+#include "G4OpenGLStoredX.hh"
 #endif
 
 #ifdef G4VIS_USE_OPENGLWIN32
@@ -77,26 +75,22 @@
 #include "G4OpenGLStoredWin32.hh"
 #endif
 
+#ifdef G4VIS_USE_OPENGLXM
+#include "G4OpenGLImmediateXm.hh"
+#include "G4OpenGLStoredXm.hh"
+#endif
+
 #ifdef G4VIS_USE_OIX
-#include <G4OpenInventorX.hh>
+#include "G4OpenInventorX.hh"
 #endif
 
 #ifdef G4VIS_USE_OIWIN32
-#include <G4OpenInventorWin32.hh>
-#endif
-
-#ifdef G4VIS_USE_RAYTRACER
-#include "G4RayTracer.hh"
+#include "G4OpenInventorWin32.hh"
 #endif
 
 #ifdef G4VIS_USE_VRML
-#include <G4VRML1.hh>
-#include <G4VRML2.hh>
-#endif
-
-#ifdef G4VIS_USE_VRMLFILE
-#include <G4VRML1File.hh>
-#include <G4VRML2File.hh>
+#include "G4VRML1.hh"
+#include "G4VRML2.hh"
 #endif
 
 //_____________________________________________________________________________
@@ -143,27 +137,26 @@ void TG4VisManager::RegisterGraphicsSystems()
 
   // all created graphics system instances are
   // deleted in G4VisManager::~G4VisManager()
+
+  // Graphics Systems not needing external packages or libraries...
+  RegisterGraphicsSystem (new G4ASCIITree);
+  RegisterGraphicsSystem (new G4DAWNFILE);
+  RegisterGraphicsSystem (new G4GAGTree);
+  RegisterGraphicsSystem (new G4HepRepFile);
+  RegisterGraphicsSystem (new G4HepRep);
+  RegisterGraphicsSystem (new G4RayTracer);
+  RegisterGraphicsSystem (new G4VRML1File);
+  RegisterGraphicsSystem (new G4VRML2File);
+
+  // Graphics systems needing external packages or libraries...
+
 #ifdef G4VIS_USE_DAWN
-  RegisterGraphicsSystem(new G4FukuiRenderer);
-#endif
-
-#ifdef G4VIS_USE_DAWNFILE
-  RegisterGraphicsSystem(new G4DAWNFILE);
-#endif
-
-#ifdef G4VIS_USE_OPACS
-  RegisterGraphicsSystem(new G4Wo);
-  RegisterGraphicsSystem(new G4Xo);
+  RegisterGraphicsSystem (new G4FukuiRenderer);
 #endif
 
 #ifdef G4VIS_USE_OPENGLX
-  RegisterGraphicsSystem(new G4OpenGLImmediateX);
-  RegisterGraphicsSystem(new G4OpenGLStoredX);
-#endif
-
-#ifdef G4VIS_USE_OPENGLXM
-  RegisterGraphicsSystem(new G4OpenGLImmediateXm);
-  RegisterGraphicsSystem(new G4OpenGLStoredXm);
+  RegisterGraphicsSystem (new G4OpenGLImmediateX);
+  RegisterGraphicsSystem (new G4OpenGLStoredX);
 #endif
 
 #ifdef G4VIS_USE_OPENGLWIN32
@@ -171,26 +164,22 @@ void TG4VisManager::RegisterGraphicsSystems()
   RegisterGraphicsSystem (new G4OpenGLStoredWin32);
 #endif
 
+#ifdef G4VIS_USE_OPENGLXM
+  RegisterGraphicsSystem (new G4OpenGLImmediateXm);
+  RegisterGraphicsSystem (new G4OpenGLStoredXm);
+#endif
+
 #ifdef G4VIS_USE_OIX
-  RegisterGraphicsSystem(new G4OpenInventorX);
+  RegisterGraphicsSystem (new G4OpenInventorX);
 #endif
 
 #ifdef G4VIS_USE_OIWIN32
-  RegisterGraphicsSystem(new G4OpenInventorWin32);
-#endif
-
-#ifdef G4VIS_USE_RAYTRACER
-  RegisterGraphicsSystem (new G4RayTracer);
+  RegisterGraphicsSystem (new G4OpenInventorWin32);
 #endif
 
 #ifdef G4VIS_USE_VRML
-  RegisterGraphicsSystem(new G4VRML1);
-  RegisterGraphicsSystem(new G4VRML2);
-#endif
-
-#ifdef G4VIS_USE_VRMLFILE
-  RegisterGraphicsSystem(new G4VRML1File);
-  RegisterGraphicsSystem(new G4VRML2File);
+  RegisterGraphicsSystem (new G4VRML1);
+  RegisterGraphicsSystem (new G4VRML2);
 #endif
 
   if (fVerboseLevel > 0) {
