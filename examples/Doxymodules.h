51ca78091e82cd1b980edb800021ab24af7ab41c
Ivana Hrivnacova <ihrivnac@mail.cern.ch>
2008-01-09 15:45:04 +0000

    Adding files for Doxygen documentation

---
+++ examples/Doxymodules.h
@@ -0,0 +1,60 @@
+// $Id$
+
+//------------------------------------------------
+// The Geant4 Virtual Monte Carlo package
+// Copyright (C) 2007, Ivana Hrivnacova
+// All rights reserved.
+//
+// For the licensing terms see geant4_vmc/LICENSE.
+// Contact: vmc@pcroot.cern.ch
+//-------------------------------------------------
+
+/// \file Doxymodules.h
+/// \brief The definitions examples modules for Doxygen
+
+/** \defgroup E01 E01
+ *  \brief The VMC example E01
+ *
+ *  The Geant4 ExampleN01 adapted to Virtual Monte Carlo. 
+ *  See more on the \ref README_E01 page.
+ *  \{
+ */
+ 
+/** \} */
+
+/** \defgroup E02 E02
+ *  \brief The VMC example E02
+ *
+ *  The Geant4 ExampleN02 adapted to Virtual Monte Carlo. 
+ *  See more on the \ref README_E02 page.
+ *  \{
+ */
+/** \} */
+
+/** \defgroup E03 E03
+ *  \brief The VMC example E03
+ *
+ *  The Geant4 ExampleN03 adapted to Virtual Monte Carlo
+ *  and extended for testing some other VMC features. 
+ *  See more on the \ref README_E03 page.
+ *  \{
+ */
+/** \} */
+
+/** \defgroup E06 E06
+ *  \brief The VMC example E06
+ *
+ *  The Geant4 ExampleN06 adapted to Virtual Monte Carlo. 
+ *  See more on the \ref README_E06 page.
+ *  \{
+ */
+/** \} */
+
+
+/** \defgroup Tests Tests
+ *
+ *  The VMC examples test macros
+ *  \{
+ */
+/** \} */
+
