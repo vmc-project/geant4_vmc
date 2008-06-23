78aca389fa1c77f386c2ce42e211e3b6d5b3620e
Ivana Hrivnacova <ihrivnac@mail.cern.ch>
2008-06-23 15:35:26 +0000

    Implemented user defined regions

---
+++ examples/E03/g4Config3.C
@@ -0,0 +1,44 @@
+// $Id: g4Config1.C 341 2008-05-26 11:04:57Z ivana $
+
+//------------------------------------------------
+// The Virtual Monte Carlo examples
+// Copyright (C) 2007, 2008 Ivana Hrivnacova
+// All rights reserved.
+//
+// For the licensing terms see geant4_vmc/LICENSE.
+// Contact: vmc@pcroot.cern.ch
+//-------------------------------------------------
+
+/// \ingroup E03
+/// \file E03/g4Config1.C
+/// \brief Configuration macro for Geant4 VirtualMC for Example03
+///
+/// Demonstrates activation of user defined regions
+
+void Config()
+{
+/// The configuration function for Geant4 VMC for Example03
+/// called during MC application initialization. 
+/// For geometry defined with Geant4 in a user run configuration.
+
+  // Load G4 novice N03 example library (with detector construction)
+  gSystem->Load("$G4INSTALL/tmp/$G4SYSTEM/exampleN03/libexampleN03");
+
+  // Load library with a user run configuration
+  gSystem->Load("libgeant4e03");
+
+  // Run configuration with user defined regions
+  Ex03RunConfiguration3* runConfiguration 
+    = new Ex03RunConfiguration3("geomRootToGeant4");
+
+  // TGeant4
+  TGeant4* geant4
+    = new TGeant4("TGeant4", "The Geant4 Monte Carlo", runConfiguration);
+
+  // Customise Geant4 setting
+  // (verbose level, global range cut, ..)
+  geant4->ProcessGeantMacro("g4config.in");
+  geant4->ProcessGeantCommand("/mcVerbose/composedPhysicsList 2");
+
+  cout << "Geant4 has been created." << endl;
+}
