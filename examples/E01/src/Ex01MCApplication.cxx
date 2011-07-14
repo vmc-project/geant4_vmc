847793b9b469bc64b1083a4f11fa9c5cbedc0c07
Ivana Hrivnacova <ihrivnac@mail.cern.ch>
2011-07-14 16:47:00 +0000

    In examples/E01: Removing test for TVirtualMC::Gfmate (not available in Root 5.30/00); putting test for new TVirtualMC::GetMaterial() within a condition on Root version (to keep the example compiling with Root 5.28/00x)

--- examples/E01/src/Ex01MCApplication.cxx
+++ examples/E01/src/Ex01MCApplication.cxx
@@ -475,6 +475,7 @@ void Ex01MCApplication::TestVMCGeometryGetters()
   Int_t imat = 2;
   Double_t a, z, density, radl, inter;
   TArrayD mpar;
+#if ROOT_VERSION_CODE >= ROOT_VERSION(5,30,0)
   result = gMC->GetMaterial(imat, matName, a, z, density, radl, inter, mpar);
   if (result) {
     cout << "Material with ID " << imat << ": " << endl;
@@ -496,6 +497,7 @@ void Ex01MCApplication::TestVMCGeometryGetters()
     cerr << "Material with ID " << imat << " not found" << endl;
   }
   cout << endl;
+#endif
 
   // Get material by volume name
   //
@@ -596,44 +598,5 @@ void Ex01MCApplication::TestVMCGeometryGetters()
     cout << " not found" << endl;
   else 
     cout << " found" << endl;
-
-  // Gfmate -  float
-  // Now deprecated in VMC
-  char name2[21]; 
-  Float_t a2, z2, dens2, radl2, absl2;
-  Int_t nbuf2;
-  Float_t ubuf2[10];
-  gMC->Gfmate(2, name2, a2, z2, dens2, radl2, absl2, ubuf2, nbuf2);
-
-  cout << "Gfmate(float): imat = 2:" << endl;
-  cout << "  name = " << name2 << " a = " << a2 << " z = " << z2 
-       << " dens = " << dens2 << " radl = " << radl2 
-       << " absl = " << absl2 << endl;
-
-  cout << "  ubuf = ";
-  if ( nbuf2 == 0 ) 
-    cout << "not defined";
-  else 
-    for ( Int_t j=0; j<nbuf2; j++ ) cout << ubuf2[j] << " ";
-  cout << endl;   
-
-  // Gfmate -  double
-  // Now deprecated in VMC
-  char name3[21]; 
-  Double_t a3, z3, dens3, radl3, absl3;
-  Int_t nbuf3;
-  Double_t ubuf3[10];
-  gMC->Gfmate(1, name3, a3, z3, dens3, radl3, absl3, ubuf3, nbuf3);
-  cout << "Gfmate(double): imat = 1:" << endl;
-  cout << "  name = " << name3 << " a = " << a3 << " z = " << z3 
-       << " dens = " << dens3 << " radl = " << radl3 
-       << " absl = " << absl3 << endl;
-
-  cout << "  ubuf = ";
-  if ( nbuf3 == 0 ) 
-    cout << "not defined";
-  else 
-    for ( Int_t j=0; j<nbuf3; j++ ) cout << ubuf3[j] << " ";
-  cout << endl;   
  }  
 
