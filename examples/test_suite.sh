017d017036c4c61c449471617201653edf855ed1
Ivana Hrivnacova <ihrivnac@mail.cern.ch>
2012-12-10 13:43:57 +0000

    Adding A01 example in run_suite.sh and test_suite.sh

--- examples/test_suite.sh
+++ examples/test_suite.sh
@@ -27,7 +27,7 @@ if [ "$TESTG3" = "1" -a  "$TESTG4" = "1" ]; then
   rm -fr $OUTDIR
 fi  
 
-for EXAMPLE in E01 E02 E03 E06
+for EXAMPLE in E01 E02 E03 E06 A01
 do
   OUT=$OUTDIR/$EXAMPLE
   if [ ! -d $OUT ]; then
@@ -38,7 +38,7 @@ do
 
   echo "... Example $EXAMPLE"
   
-  if [ "$EXAMPLE" != "E03" ]; then 
+  if [ "$EXAMPLE" != "E03" -a "$EXAMPLE" != "A01" ]; then 
   
     if [ "$TESTG3" = "1" ]; then
       echo "... Running test with G3, geometry via TGeo, TGeo navigation" 
@@ -64,8 +64,9 @@ do
       echo "... Running test with G4, geometry via VMC,  TGeo navigation" 
       root.exe -q "test_$EXAMPLE.C(\"g4tgeoConfigOld.C\", kTRUE)" >& $OUT/test_g4_vmc_tgeo.out   
     fi  
+  fi  
 
-  else
+  if [ "$EXAMPLE" = "E03" ]; then 
     # Run three macro + special configuration available only in E03 example
     if [ "$TESTG3" = "1" ]; then
       echo "... Running test with G3, geometry via TGeo, TGeo navigation" 
@@ -153,8 +154,25 @@ do
       root.exe -q "test_E03_5.C(\"g4Config2.C\", kFALSE)" >& tmpfile    
       cat tmpfile >> $OUT/test_g4_tgeo_nat_pl.out
       rm tmpfile
+    fi 
+  fi   
+
+  if [ "$EXAMPLE" = "A01" ]; then 
+    if [ "$TESTG3" = "1" ]; then
+      echo "... Running test with G3, geometry via TGeo, TGeo navigation" 
+      root.exe -q "test_$EXAMPLE.C(\"g3tgeoConfig.C\", kFALSE)" >& $OUT/test_g3_tgeo_tgeo.out   
     fi  
 
+    if [ "$TESTG4" = "1" ]; then
+      echo "... Running test with G4, geometry via TGeo, Native navigation" 
+      root.exe -q "test_$EXAMPLE.C(\"g4Config.C\", kFALSE)" >& $OUT/test_g4_tgeo_nat.out   
+ 
+      echo "... Running test with G4, geometry via TGeo, TGeo navigation" 
+      root.exe -q "test_$EXAMPLE.C(\"g4tgeoConfig.C\", kFALSE)" >& $OUT/test_g4_tgeo_tgeo.out   
+
+      echo "... Running test with G4, geometry via G4, Native navigation" 
+      root.exe -q "test_$EXAMPLE.C(\"g4Config1.C\", kFALSE)" >& $OUT/test_g4_tgeo_tgeo.out   
+    fi  
   fi  
 
   echo " "
