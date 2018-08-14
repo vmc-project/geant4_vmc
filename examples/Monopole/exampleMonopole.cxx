0860cdc8ece32d93c030eb8cc0c54304b0dce01d
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2018-08-14 23:16:29 +0200

    Updated example program and removed test macro and test program

--- examples/Monopole/exampleMonopole.cxx
+++ examples/Monopole/exampleMonopole.cxx
@@ -18,14 +18,11 @@
 /// \author I. Hrivnacova; IPN, Orsay
 
 #include "MCApplication.h"
+#include "DetectorConstruction.h"
 
 #ifdef USE_GEANT4
 #include "TG4RunConfiguration.h"
 #include "TGeant4.h"
-#endif
-
-#ifdef USE_GEANT3
-#include "TGeant3TGeo.h"
 #endif
 
 #include "TThread.h"
@@ -34,6 +31,12 @@
 /// Application main program
 int main(int argc, char** argv)
 {
+#ifdef USE_GEANT3
+  // This example cannot run with Geant3
+  std::cerr << "Monopole example: Geant3 is not supported." << std::endl;
+  return 1;
+#endif
+  
   // Initialize Root threading.
   // (Multi-threading is triggered automatically if Geant4 was built 
   //  in MT mode.)
@@ -46,31 +49,45 @@ int main(int argc, char** argv)
   VMC::Monopole::MCApplication* appl 
     =  new VMC::Monopole::MCApplication("ExampleMonopole",
                                         "The exampleMonopole MC application");
+  // Set detector parameters
+  // /testex/det/setMat G4_Si
+  // /testex/det/setSizeX  10 cm
+  // /testex/det/setSizeYZ 20 cm
+  // /testex/det/setStepSize 0.2 mm
+  // /testex/run/binSize 0.2 mm
+  VMC::Monopole::DetectorConstruction* detector = appl->GetDetectorConstruction();
+  detector->SetAbsorberMaterial("Si");
+  detector->SetAbsorberSizeX(10);
+  detector->SetAbsorberSizeYZ(20);
+  detector->SetMaxStepSize(0.02);
+  appl->SetBinSize(0.02);
 
 #ifdef USE_GEANT4
   // RunConfiguration for Geant4 
   TG4RunConfiguration* runConfiguration 
-    = new TG4RunConfiguration("geomRootToGeant4");
+    = new TG4RunConfiguration("geomRootToGeant4", "FTFP_BERT+monopole", "stepLimiter");
 
   // TGeant4
   TGeant4* geant4
     = new TGeant4("TGeant4", "The Geant4 Monte Carlo", runConfiguration,
                   argc, argv);
+  std::cout << "Geant4 has been created." << std::endl;
 
   // Customise Geant4 setting
   // (verbose level, global range cut, ..)
-  // geant4->ProcessGeantMacro("g4config.in");
-  geant4->ProcessGeantCommand("/mcVerbose/all 1");  
+  geant4->ProcessGeantMacro("g4config.in");  
 #endif
   
-#ifdef USE_GEANT3
-  TGeant3* geant3 = new TGeant3TGeo("C++ Interface to Geant3");
-  geant3->SetHADR(0);
-#endif
-
   // Run from this main
   appl->InitMC("");
-  appl->RunMC(1);
+
+#ifdef USE_GEANT4
+  // Customise Geant4 setting after initialization:
+  // Physics list
+  geant4->ProcessGeantMacro("g4config2.in");
+#endif
+
+  appl->RunMC(100);
 
   delete appl;
 }  
