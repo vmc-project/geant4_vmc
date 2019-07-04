2c4107c4012cf3fff96c9700f0596d8e8c4310a6
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2019-07-04 16:12:04 +0200

    Making version v3-6-p4

--- source/global/include/TG4Version.h
+++ source/global/include/TG4Version.h
@@ -15,7 +15,7 @@
 ///
 /// \author I. Hrivnacova; IPN, Orsay
 
-#define GEANT4_VMC_RELEASE "3.6.p3"
-#define GEANT4_VMC_RELEASE_DATE "9 April 2019"
+#define GEANT4_VMC_RELEASE "3.6.p4"
+#define GEANT4_VMC_RELEASE_DATE "4 July 2019"
 
 #endif // TG4_VERSION_H
