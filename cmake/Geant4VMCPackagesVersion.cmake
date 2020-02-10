95e42bb34dc3883d241ede2361d6d775e88460ae
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-02-10 17:13:38 +0100

    Making version v3-6-p6

--- cmake/Geant4VMCPackagesVersion.cmake
+++ cmake/Geant4VMCPackagesVersion.cmake
@@ -13,6 +13,6 @@
 
 set(Geant4VMCPackages_VERSION_MAJOR "3")
 set(Geant4VMCPackages_VERSION_MINOR "6")
-set(Geant4VMCPackages_VERSION_PATCH "5")
+set(Geant4VMCPackages_VERSION_PATCH "6")
 set(Geant4VMCPackages_VERSION
     ${Geant4VMCPackages_VERSION_MAJOR}.${Geant4VMCPackages_VERSION_MINOR}.${Geant4VMCPackages_VERSION_PATCH})
