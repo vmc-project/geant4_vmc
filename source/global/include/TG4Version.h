eb01196cf325f917ac1d42a6496bdc23e8c44aac
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-04-30 14:47:55 +0200

    Making version 5.0.p3

--- source/global/include/TG4Version.h
+++ source/global/include/TG4Version.h
@@ -15,7 +15,7 @@
 ///
 /// \author I. Hrivnacova; IPN, Orsay
 
-#define GEANT4_VMC_RELEASE "5.0.p2"
-#define GEANT4_VMC_RELEASE_DATE "9 April 2020"
+#define GEANT4_VMC_RELEASE "5.0.p3"
+#define GEANT4_VMC_RELEASE_DATE "30 April 2020"
 
 #endif // TG4_VERSION_H
