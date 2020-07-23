9a709135737b0d5b061f2c484aecd76867f603a7
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2020-07-23 15:22:40 +0200

    Making version v3-6-p6-inclxx-biasing-p3

--- source/global/include/TG4Version.h
+++ source/global/include/TG4Version.h
@@ -15,7 +15,7 @@
 ///
 /// \author I. Hrivnacova; IPN, Orsay
 
-#define GEANT4_VMC_RELEASE "3.6.p6.inclxx-biasing-p2"
-#define GEANT4_VMC_RELEASE_DATE "10 February 2020"
+#define GEANT4_VMC_RELEASE "3.6.p6.inclxx-biasing-p3"
+#define GEANT4_VMC_RELEASE_DATE "23 July 2020"
 
 #endif // TG4_VERSION_H
