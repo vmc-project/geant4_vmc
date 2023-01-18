5abe1db376128db53519b90e59e6d7d6403a614b
Ivana Hrivnacova <Ivana.Hrivnacova@cern.ch>
2023-01-18 12:05:30 +0100

    Making version 6.2

--- source/global/include/TG4Version.h
+++ source/global/include/TG4Version.h
@@ -15,7 +15,7 @@
 ///
 /// \author I. Hrivnacova; IPN, Orsay
 
-#define GEANT4_VMC_RELEASE "6.1"
-#define GEANT4_VMC_RELEASE_DATE "17 March 2022"
+#define GEANT4_VMC_RELEASE "6.2"
+#define GEANT4_VMC_RELEASE_DATE "18 January 2023"
 
 #endif // TG4_VERSION_H
